3fb9f414cc90bbc0d861c2daa7d8795d5d9282a1
Sonu <bilawal.jabbar.786@gmail.com>
2021-12-22 16:18:08 +0500

    new

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -875,7 +875,7 @@ $gs = \App\GeneralSettings::find(1);
                 )
             )
                 var d = "whatsapp://send";
-            var g = d + "?phone=+590690283282" + e + f;
+            var g = d + "?phone=+590690425402" + e + f;
             window.open(g, "_blank");
         }
     }),
